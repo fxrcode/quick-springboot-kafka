7329044470ecc9953484edd594041c045f602da5
fxrcode <fxrcode@gmail.com>
2021-01-23 16:40:14 -0800

    init javaguide springboot-kafka quickstart

---
+++ kafka-quickstart/src/main/java/com/javaguide/kafkaquickstart/KafkaQuickstartApplication.java
@@ -0,0 +1,13 @@
+package com.javaguide.kafkaquickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaQuickstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaQuickstartApplication.class, args);
+	}
+
+}
---
+++ kafka-quickstart/src/test/java/com/javaguide/kafkaquickstart/KafkaQuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaguide.kafkaquickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaQuickstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
